c0df24203a1d610cb49a472ffcdd726ce6aabea0
dore <promptoattano@gmail.com>
2023-01-09 17:50:48 -0300

    Estilização da página start

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { StatusBar } from 'react-native';
 import Routes from "./src/Routes";
 
 export default function App(){
-  return (
-  <>
-   <StatusBar barStyle={"auto"}/>
-   <Routes/>
+	return (
+		<>
+		<StatusBar barStyle={"auto"} />
+		<Routes />	
 
-  </>
-  );
+		</>
+	);
 }
--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -1,7 +1,7 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Stack from "../Pages/Start";
+import Start from "../Pages/Start";
 
 const Stack = createNativeStackNavigator();
 
