8b2b0acb101eddd5329c62e371be104c00186594
dore <promptoattano@gmail.com>
2023-01-11 19:55:19 -0300

    Feat: Nova página com as 4 funcionalidades do APP

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native-gesture-handler";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
 export default function AppExplanation(){
 
-    function handleSetShowHomeButton(){
+    function handleSetShowHome(){
         console.log("Botão clicado 2!")
     }
 
     return(
-        <View style={style.container}>
+        <View style={styles.container}>
             <ScrollView>
                 <View style={{alignItems: "center"}}>
 
                     <Text style={styles.title}>
                         Antes, deixa {"\n"} eu te explicar...
                     </Text>
+
+                    <ExplanationCard/>
 
                     <Text style={styles.descriptionCta}>
                         Pronto(a) para subir de nível na vida?
--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -2,6 +2,7 @@ import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Start from "../Pages/Start";
+import AppExplanation from "../Pages/AppExplanation";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function AllPages(){
             }}
             >
 
+            <Stack.Screen name="AppExplanation" component={AppExplanation} />
             <Stack.Screen name="Start" component={Start} />
             </Stack.Navigator>
 
